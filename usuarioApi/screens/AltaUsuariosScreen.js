578212b60e1333f973d8c61965797b9c736a7e7e
Emiliano Ledesma <emilianoledesmaledesma@outlook.com>
2026-07-28 12:02:38 -0600

    Agregar funcionalidad de CORS y mejorar la gestión de usuarios en las pantallas de alta y consulta

--- usuarioApi/screens/AltaUsuariosScreen.js
+++ usuarioApi/screens/AltaUsuariosScreen.js
@@ -1,9 +1,52 @@
 import React, { useState } from 'react';
-import {View,SafeAreaView,Text,TextInput,Pressable,StyleSheet,} from 'react-native';
+import {View,SafeAreaView,Text,TextInput,Pressable,StyleSheet,Alert,Platform,} from 'react-native';
 
 export default function AltaUsuariosScreen() {
   const [nombre, setNombre] = useState('');
   const [edad, setEdad] = useState('');
+  const [cargando, setCargando] = useState(false);
+
+  const mostrarMensaje = (titulo,mensaje) =>{
+    if (Platform.OS === 'web'){
+      window.alert(`${titulo}\n\n${mensaje}`);
+    }else{
+      Alert.alert(titulo,mensaje);
+    }
+  }
+
+  const guardarUsuario = async() => {
+    if(nombre.trim() === '' || edad.trim() === '') {
+      mostrarMensaje('Error','Complete todos los campos.');
+      return;
+    }
+    
+    try{
+      setCargando(true);
+      const respuesta = await fetch('http://10.16.72.36:5000/v1/usuarios/',
+        {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({nombre: nombre, edad: Number(edad)}),
+        }
+      );
+      const datos = await respuesta.json();
+      console.log(datos)
+      mostrarMensaje("Exito","Usuario Registrado")
+
+      setNombre('');
+      setEdad('');
+
+    }catch (error){
+      mostrarMensaje('Error','Ocurrio un error al registrar el usuario');
+      console.log(error);
+    }
+    finally{
+      setCargando(false)
+    }
+  
+  }
 
   return (
     <SafeAreaView style={styles.container}>
@@ -29,9 +72,9 @@ export default function AltaUsuariosScreen() {
           onChangeText={setEdad}
         />
 
-        <Pressable style={styles.boton}>
+        <Pressable style={styles.boton} onPress={guardarUsuario} disabled={cargando}>
           <Text style={styles.textoBoton}>
-            Agregar Usuario
+           {cargando ? "Guardando...":"Agregar Usuario"} 
           </Text>
         </Pressable>
 
--- usuarioApi/screens/ConsultaUsuariosScreen.js
+++ usuarioApi/screens/ConsultaUsuariosScreen.js
@@ -1,16 +1,25 @@
-import React from 'react';
-import {SafeAreaView,View,Text,FlatList,StyleSheet,
-} from 'react-native';
+import React, { useEffect, useState } from 'react';
+import {SafeAreaView,View,Text,FlatList,StyleSheet,} from 'react-native';
 
 export default function ConsultaUsuariosScreen() {
 
-  const usuarios = [
-    { id: '1', nombre: 'Isay Guerra', edad: 22 },
-    { id: '2', nombre: 'Ana López', edad: 19 },
-    { id: '3', nombre: 'Carlos Gonzalez', edad: 25 },
-    { id: '4', nombre: 'Bjork Guerra', edad: 21 },
-    { id: '5', nombre: 'Luisa Martínez', edad: 28 },
-  ];
+  const [usuarios, setUsuarios] = useState([]);
+
+  const obtenerUsuarios = async () => {
+    try {
+      const respuesta = await fetch('http://10.16.72.36:5000/v1/usuarios/');
+      const datos = await respuesta.json();
+      console.log(datos);
+
+      setUsuarios(datos.usuarios);
+    }catch (error){
+      console.error('Error al obtener los usuarios:', error);
+    }
+  }
+
+  useEffect (() => {
+    obtenerUsuarios();
+  }, []);
 
   const renderTarjeta = ({ item }) => (
     <View style={styles.card}>
